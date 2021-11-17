ef4e025a708b680dbd0a408e1996f51408352dd6
vlkl-sap <71723302+vlkl-sap@users.noreply.github.com>
2021-11-17 22:06:22 +0100

    replace with a cruder version that actually works

--- delombok.sh
+++ delombok.sh
@@ -1,47 +1,11 @@
 #!/bin/bash
 
 set -e
+#set -x
 
-# delombok if necessary
-if git grep -q "^import lombok" '*.java'; then
-  echo "[+] Downloading lombok..."
-  curl https://projectlombok.org/downloads/lombok.jar -o "$GITHUB_WORKSPACE/lombok.jar"
+mvn package org.projectlombok:lombok-maven-plugin:delombok -DskipTests -Dlombok.verbose=true -Dlombok.addOutputDirectory=false -Dlombok.sourceDirectory=src/main/java
 
-  # Identify class path to use for Lombok
-  CLASSPATH=""
-
-  # For each build system, correctly build out a list of `classpath`'s 
-  if [[ -f "pom.xml" ]]; then
-    # dependency:build-classpath will also restore the dependencies, as well as printing them to the file
-    mvn compile dependency:build-classpath -B \
-      -Dmdep.outputFile=lombok.classpath \
-      --no-transfer-progress
-
-    CLASSPATH=$(cat lombok.classpath)
-  fi
-
-  function mergeDelombok {
-    DIFF=$(diff -Z -w -b -B --unified=200000 --minimal "$GITHUB_WORKSPACE/$1" "$1")
-    if [ $? -ne 0 ]; then
-     echo "Ran delombok on $1"
-     echo "$DIFF" | sed 's/^ //g' | sed 's/^-.*$//g' | sed -r 's#(.*)//(.*)$#\1/*\2/**/#g' | sed ':a;N;$!ba;s/\n+/ /g' | tail -n +3 | grep -v '\ No newline at end of file' > "$GITHUB_WORKSPACE/$1"
-    fi
-  }
-  export -f mergeDelombok
-
-  java -jar "$GITHUB_WORKSPACE/lombok.jar" \
-    delombok \
-    -f suppressWarnings:skip \
-    -f generated:skip \
-    -f generateDelombokComment:skip \
-    --classpath="$CLASSPATH" \
-    -n --onlyChanged \
-    . -d "$GITHUB_WORKSPACE/delombok"
-
-  pushd "$GITHUB_WORKSPACE/delombok"
-  find . -name '*.java' -exec bash -c 'mergeDelombok "{}"' \;
-  popd
-
-else
-  echo "[!] No lombok code detected"
-fi
+find -name delombok | while read line;
+do
+cp -r "$line"/* "${line%/target/generated-sources/delombok}/src/main/java/"
+done
